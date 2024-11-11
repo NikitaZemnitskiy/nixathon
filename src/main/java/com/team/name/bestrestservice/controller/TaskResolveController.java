1acbd70891828f350d85148c21e1f7700281ba85
nzemnytskyi <nikita.zemnytskyi@cengage.com>
2024-11-12 01:37:20 +0200

    Heroku test

--- src/main/java/com/team/name/bestrestservice/controller/TaskResolveController.java
+++ src/main/java/com/team/name/bestrestservice/controller/TaskResolveController.java
@@ -1,14 +1,18 @@
 package com.team.name.bestrestservice.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
 public class TaskResolveController {
 
-    @GetMapping("/healthz")
-    public ResponseEntity<String> checkHealth() {
-        return ResponseEntity.ok("OK");
+    @GetMapping(value = "/healthz", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, String>> checkHealth() {
+        return ResponseEntity.ok(Collections.singletonMap("status", "OK"));
     }
 }
